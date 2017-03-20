fd517e893eb4a8809ee5a4c77bc9f551a7c36b2f
yanling.liao <yanling.liao@ele.me>
2017-03-20 12:50:18 +0800

    添加基础组件

---
+++ vue/src/components/Form/FormButton.vue
@@ -0,0 +1,16 @@
+<template>
+  <el-form-item>
+    <el-button type="primary" @click="$emit('onSubmit')">立即创建</el-button>
+    <el-button>取消</el-button>
+  </el-form-item>
+</template>
+
+<script>
+  import { FormItem, Button } from 'element-ui'
+  export default {
+    components: {
+      ElFormItem: FormItem,
+      ElButton: Button
+    }
+  }
+</script>
--- vue/src/router/Home/index.vue
+++ vue/src/router/Home/index.vue
@@ -1,19 +1,43 @@
 <template>
   <main>
+    <el-header></el-header>
+    <!--<el-sidebar style="width: 200px;"></el-sidebar>-->
     <el-button type="primary" @click.native="hello">primary</el-button>
-    <hello></hello>
+    <el-form label-width="80px" :model="form">
+      <my-input label="jjjj" name="aaa" v-model="form.aa"></my-input>
+      <my-select label="ddd" name="ddd" :options="selectData" v-model="form.bb"></my-select>
+      <my-button @onSubmit="onSubmit"></my-button>
+    </el-form>
   </main>
 </template>
 
 <script>
-  import { Button, Message } from 'element-ui'
-
-  import Hello from 'components/Hello'
-
+  import { Button, Message, Form } from 'element-ui'
+  import Header from '../../components/Header'
+  import Sidebar from '../../components/Sidebar'
+  import FormInput from '../../components/Form/FormInput'
+  import FormSelect from '../../components/Form/FormSelect'
+  import FormButton from '../../components/Form/FormButton'
   export default {
     components: {
       ElButton: Button,
-      Hello
+      ElHeader: Header,
+      ElSidebar: Sidebar,
+      FormInput,
+      FormSelect,
+      FormButton
+    },
+    data () {
+      return {
+        form: {
+          aa: '',
+          bb: ''
+        },
+        selectData: [{
+          label: '11',
+          value: '111'
+        }]
+      }
     },
     methods: {
       hello () {
