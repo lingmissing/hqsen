750c58558b78636a4ee37ba01152725cb09cd083
yanling.liao <yanling.liao@ele.me>
2018-03-17 21:36:51 +0800

    修改线下齐全

--- react_pro/src/components/MyCard/MyCard.js
+++ react_pro/src/components/MyCard/MyCard.js
@@ -10,12 +10,12 @@ class MyCard extends Component {
     return (
       <div className="my-card">
         <div className="card-header">
-          {data.create_time} （{data.status_type === '1' ? '总经理' : '财务'}审批）
+          {data.create_time} （{data.status_type === '1' ? '总经理' : '财务'}审批{data.status === '6'
+            ? '-线下齐全'
+            : ''}）
           <span className="card-status">{data.status_view}</span>
         </div>
-        <div className="card-body">
-          审批备注：{data.status_desc}
-        </div>
+        <div className="card-body">审批备注：{data.status_desc}</div>
       </div>
     )
   }
